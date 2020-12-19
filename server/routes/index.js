ec68e00bb3cc7d0cee36634214b9d78e09d1d68e
Raúl López <rldona@gmail.com>
2020-12-19 11:43:39 +0100

    first commit

--- server/routes/index.js
+++ server/routes/index.js
@@ -19,7 +19,7 @@ module.exports = function(app) {
     let mediaList = [];
 
     db.collection(type)
-      .orderBy(filter)
+      .orderBy(filter, 'desc')
       .startAt(start)
       .limit(limit)
       .get()
